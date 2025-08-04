57e104eb8843b84605c10e2d3e747b1d7868d137
trianandafajar <mail.trianandafajar@gmail.com>
2025-08-04 09:50:13 +0700

    Update Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,11 +18,14 @@ const config: Config = {
           600: '#2F6FEB',
         },
       },
-    },
-    keyframes: {
-      shimmer: {
-        '100%': {
-          transform: 'translateX(100%)',
+      animation: {
+        shimmer: 'shimmer 2s infinite',
+      },
+      keyframes: {
+        shimmer: {
+          '100%': {
+            transform: 'translateX(100%)',
+          },
         },
       },
     },
